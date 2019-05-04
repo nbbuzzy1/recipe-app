aa93828fa97ad166de8ca739e46df5799a03f6c2
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-05-04 08:13:42 -0400

    updated recipe service to add ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,38 @@
 import { Recipe } from './recipe.model'
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
-  
+
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.thepacker.com/sites/default/files/sonic-slinger.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.thepacker.com/sites/default/files/sonic-slinger.jpg')
+    new Recipe(
+      'Tasty Schnitzel', 
+      'A super-tasty schnitzel - just awesome!', 
+      'https://tinykitchentv.com/wp-content/uploads/2018/09/FI2-Schnitzel-Tiny-Kitchen-Big-Taste.jpg', 
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Sonic Burger', 
+      'Decent fast food burger', 
+      'https://www.thepacker.com/sites/default/files/sonic-slinger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
   ]; 
+
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients)
   }
 
 }
