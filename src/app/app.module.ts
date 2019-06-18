b192ddee4d95dce98b4bb37168f63169b263fb60
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-06-18 11:20:29 -0400

    added lazy loading for recipes, shopping-list, and auth modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.module';
-import { RecipesRoutingModule } from './recipes/recipes-routing.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { ShoppingListRoutingModule } from './shopping-list/shopping-list-routing.module';
+// import { ShoppingListModule } from './shopping-list/shopping-list.module';
+// import { ShoppingListRoutingModule } from './shopping-list/shopping-list-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
+// import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
@@ -24,13 +21,11 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule,
-    RecipesRoutingModule,
-    ShoppingListModule,
-    ShoppingListRoutingModule,
+    // ShoppingListModule,
+    // ShoppingListRoutingModule,
     SharedModule,
     CoreModule,
-    AuthModule
+    // AuthModule
   ],
   bootstrap: [AppComponent],
 })
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     AuthComponent
   ],
-  imports: [CommonModule, SharedModule, FormsModule, RouterModule.forChild([{ path: 'auth', component: AuthComponent}])],
+  imports: [CommonModule, SharedModule, FormsModule, RouterModule.forChild([{ path: '', component: AuthComponent}])],
   exports: []
 })
 
